f80626b92b9667a825b54cb423940decdb7be6a1
cod3mos <contato@codemos.com.br>
2022-07-25 20:45:18 -0300

    fix: removido interface em duplicidade

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,24 +2,13 @@ import React from 'react'
 import GlobalStyle from '../../styles/globals'
 
 import type { AppProps } from 'next/app'
-import { ThemeProvider } from 'styled-components'
-
-export interface DefaultTheme {
-    theme: {
-        colors: {
-            primary: string
-            secondary: string
-        }
-    }
-}
+import { ThemeProvider, DefaultTheme } from 'styled-components'
 
 const theme: DefaultTheme = {
-    theme: {
-        colors: {
-            primary: '#111',
-            secondary: '#0070f3'
-        }
-    }
+    colors: {
+        primary: '#0D0D0D',
+        secondary: '#0070f3'
+    },
 }
 
 export default function App({ Component, pageProps }: AppProps) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import Head from 'next/head'
 import styled from 'styled-components'
 
-import { DefaultTheme } from './_app'
-
 const Title = styled.h1`
     font-size: 50px;
-    color: ${({ theme }: DefaultTheme) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.primary};
 `
 
 export default function Home() {
